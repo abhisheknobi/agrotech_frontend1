dd93d2d53accaeb177ee57e4c79d70fd772c2a2b
Abhishek Nobi <abhishek.nobi@gmail.com>
2026-04-01 14:19:18 +0530

    added config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const ML_API_URL = process.env.REACT_APP_ML_API_URL;
+const GEMINI_API = process.env.REACT_APP_GEMINI_API_KEY;
 
-const config = { BACKEND_URL, ML_API_URL };
+const config = { BACKEND_URL, ML_API_URL, GEMINI_API };
 
-export { BACKEND_URL, ML_API_URL };
+export { BACKEND_URL, ML_API_URL, GEMINI_API };
 export default config;
